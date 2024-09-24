e988f45a7bc9ab5aeeb3f6e987e4f58aa8d17b99
Dishang <dishang04@gmail.com>
2024-09-24 09:40:56 +0200

    added the game card component so now its reusable for multiple components to show certain games

--- src/app/game-card/game-card.component.ts
+++ src/app/game-card/game-card.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, input } from '@angular/core';
+import { Game } from '../game';
+import { FavoriteService } from '../favorite.service';
+import { OwnedService } from '../owned.service';
 
 @Component({
   selector: 'app-game-card',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './game-card.component.css'
 })
 export class GameCardComponent {
+  @Input() game!: Game;
+  @Input() isFavorite: boolean = false;
+  @Input() isOwned: boolean = false;
 
+  @Output() favoriteChange = new EventEmitter<boolean>();
+  @Output() ownedChange = new EventEmitter<boolean>();
+
+  constructor(public favoriteService: FavoriteService, public ownedService: OwnedService) {}
+
+  onFavoriteToggle() {
+    this.isFavorite = !this.isFavorite;
+    this.favoriteChange.emit(this.isFavorite);
+  }
+
+  onOwnedToggle() {
+    this.isOwned = !this.isOwned;
+    this.ownedChange.emit(this.isOwned);
+  }
 }
