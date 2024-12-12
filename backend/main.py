d429c7cca589cab68bf8be9102552c0ef8631a9b
Dishang <dishang04@gmail.com>
2024-12-12 14:18:05 +0100

    test

--- backend/main.py
+++ backend/main.py
@@ -14,9 +14,9 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200", "http://localhost:8000"], 
+    allow_origins=["*"], 
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
